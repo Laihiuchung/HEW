9bc8bd1f3573159297e2638bc7a8925f1b76f538
Laihiuchung <58251880+Laihiuchung@users.noreply.github.com>
2019-11-28 14:13:10 +0900

    main
    update test

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>
+
+class weapon
+{
+	
+public:
+	virtual void DMG() = 0;
+};
+
+class knife : public weapon
+{
+public:
+	void DMG()
+	{
+		std::cout << "knife weaponed" << std::endl;
+		printf("DMG+5\n");
+	}
+};
+
+class sword : public weapon
+{
+public:
+	void DMG()
+	{
+		std::cout << "sword weaponed" << std::endl;
+		printf("DMG+10\n");
+	}
+};
+
+int main()
+{
+	knife knife;
+	knife.DMG();
+	sword sword;
+	sword.DMG();
+	system("pause");
+	return 0;
+}
